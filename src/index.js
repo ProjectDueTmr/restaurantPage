f4a3075f1031d66dc9dce5de11238418f5dd787a
Nick Sanchez <nicksanchez2371@gmail.com>
2024-03-27 18:23:25 -0500

    added a title through Js

--- src/index.js
+++ src/index.js
@@ -1,4 +1,18 @@
 import _ from 'lodash';
 import './style.css';
 
+
+const header = document.querySelector('header');
+header.classList.add('banner');
+
+const title = document.createElement('h1');
+title.textContent = "Seven";
+title.classList.add('title');
+const nav = document.querySelector('nav');
+const parentElement = nav.parentNode;
+parentElement.insertBefore(title, nav);
+
+header.prepend(title);
+
+
 console.log('hey');
