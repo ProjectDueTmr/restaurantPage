adee5c53634e4cae2550c2774a6762e94ab8dece
Nick Sanchez <nicksanchez2371@gmail.com>
2024-03-28 18:39:56 -0500

    base layout for  the home tab is complete

--- src/index.js
+++ src/index.js
@@ -8,11 +8,44 @@ header.classList.add('banner');
 const title = document.createElement('h1');
 title.textContent = "Seven";
 title.classList.add('title');
-const nav = document.querySelector('nav');
-const parentElement = nav.parentNode;
-parentElement.insertBefore(title, nav);
-
 header.prepend(title);
 
 
+// HOME TAB  
+const content = document.getElementById('content');
+const homeTab = document.createElement('div');
+homeTab.classList.add('home', 'tab');
+content.append(homeTab);
+const discDiv = document.createElement('div');
+discDiv.classList.add('discription');
+homeTab.append(discDiv);
+const discPara = document.createElement('p');
+discPara.innerText = 'This is a 5 Star business. only the wealthiest of people can have access in.'
+discDiv.append(discPara);
+   
+const hoursDiv = document.createElement('div');
+ hoursDiv.classList.add('hoursDiv');
+ homeTab.append(hoursDiv);
+ const weekDays = document.createElement('p');
+ const weekends = document.createElement('p');
+ weekDays.textContent = 'Monday - Thursday: 7:00 - 1:00';
+ weekends.textContent = 'Friday - Sunday: 24/7';
+ hoursDiv.append(weekDays, weekends)
+
+ const locationDiv = document.createElement('div');
+ locationDiv.classList.add('location');
+ homeTab.append(locationDiv);
+ const locationPara = document.createElement('p');
+ locationPara.innerText = '777 Seven Ave, Sevenville, Texas';
+ locationDiv.append(locationPara);
+
+
+
+
+
+    // MENU TAB
+
+
+
+    // CONTACT TAB
 console.log('hey');
