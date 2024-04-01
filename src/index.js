7984ca54b08276ba6af0f62acd5c8ac174069652
Nick Sanchez <nicksanchez2371@gmail.com>
2024-03-31 23:38:16 -0500

    contactTab is updated with info

--- src/index.js
+++ src/index.js
@@ -83,4 +83,26 @@ menuTab.append(food);
 
 
     // CONTACT TAB
+
+    const contactTab = document.createElement('div');
+    contactTab.classList.add('contact', 'tab');
+    content.append(contactTab);
+
+    const contactTitle = document.createElement('h1');
+    contactTitle.classList.add('title');
+    contactTitle.textContent = 'Contact';
+    contactTab.append(contactTitle);
+
+    const contactTitle2 = document.createElement('h2');
+    contactTitle2.classList.add('title2');
+    contactTitle2.textContent = 'Contact us here';
+
+    const contactNum = document.createElement('p');
+    contactNum.textContent = '(777)-777-7777'
+
+    const contactEmail = document.createElement('p');
+    contactEmail.textContent = 'doesntexist@nowhere.com'
+    contactTab.append(contactTitle2, contactNum, contactEmail);
+
+
 console.log('hey');
