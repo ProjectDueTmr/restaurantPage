1b2c83e154524c6a35dff028824d6a1a9ed6d8fc
Nick Sanchez <nicksanchez2371@gmail.com>
2024-03-31 23:10:45 -0500

    added title for all sections in homeTab

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import _ from 'lodash';
 import './style.css';
 
-
+// BANNER
 const header = document.querySelector('header');
 header.classList.add('banner');
 
@@ -20,28 +20,41 @@ const homeTitle = document.createElement('h1')
 homeTitle.classList.add('title')
 homeTitle.textContent = 'Home'
 homeTab.append(homeTitle);
+
 const discDiv = document.createElement('div');
 discDiv.classList.add('discription');
 homeTab.append(discDiv);
+const discTitle = document.createElement('h2');
+discTitle.classList.add('title2');
+discTitle.textContent = 'Critic Reviews'
 const discPara = document.createElement('p');
 discPara.innerText = 'This is a 5 Star business. only the wealthiest of people can have access in.'
-discDiv.append(discPara);
+discDiv.append(discTitle, discPara);
    
+
+
 const hoursDiv = document.createElement('div');
  hoursDiv.classList.add('hoursDiv');
  homeTab.append(hoursDiv);
+const hoursTitle = document.createElement('h2');
+hoursTitle.classList.add('title2');
+hoursTitle.textContent = 'Work Hours';
+
  const weekDays = document.createElement('p');
  const weekends = document.createElement('p');
  weekDays.textContent = 'Monday - Thursday: 7:00 - 1:00';
  weekends.textContent = 'Friday - Sunday: 24/7';
- hoursDiv.append(weekDays, weekends)
+ hoursDiv.append(hoursTitle, weekDays, weekends)
 
  const locationDiv = document.createElement('div');
  locationDiv.classList.add('location');
  homeTab.append(locationDiv);
+ const locationTitle = document.createElement('h2');
+ locationTitle.classList.add('title2');
+ locationTitle.textContent = 'Where to find us';
  const locationPara = document.createElement('p');
  locationPara.innerText = '777 Seven Ave, Sevenville, Texas';
- locationDiv.append(locationPara);
+ locationDiv.append(locationTitle, locationPara);
 
 
 
